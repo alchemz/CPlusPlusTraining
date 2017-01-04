ba74c8b28b2df10016ce4c4d68ac2a05cb4dab78
xianling zhang <xianlingzhang6@gmail.com>
2017-01-03 22:47:34 -0500

    Create 4whileLoop.cpp

---
+++ 4whileLoop.cpp
@@ -0,0 +1,29 @@
+/*While loop*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	/*
+	//infinite loop
+	//while(condition)
+	while (true) {
+
+		cout << "Hello" << endl;
+
+	}
+	*/
+
+	int i = 0;//counter
+	while (i<=5)
+	{
+		cout << "Hello\n";
+		i++;
+	}
+	//without = it runs 5 times
+	//with =, it runs 6 times
+
+	return 0;
+}
