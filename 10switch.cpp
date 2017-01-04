20c8d6eb611954c9d7afb04f01c145d57e7e9a95
xianling zhang <xianlingzhang6@gmail.com>
2017-01-04 01:56:51 -0500

    Create 10switch.cpp

---
+++ 10switch.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int value=9;
+	
+	switch(value)
+	{
+	case 1:
+		value=1;
+		break;
+	case 2:
+		value=9;
+		break;
+	case 3:
+		value=10;
+		break;
+	default:
+		cout<<"";
+	}
+	
+	return 0;
+}
