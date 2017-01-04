09b0c96eaecb171a9d6c6ec2a85d8e6dd9846eeb
xianling zhang <xianlingzhang6@gmail.com>
2017-01-04 18:18:24 -0500

    Create 10Function.cpp

---
+++ 10Function.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+void checkTest(int n);
+
+int main()
+{
+
+	checkTest(10);
+
+	return 0;
+}
+
+
+void checkTest(int n)
+{
+	cout << "This is a test\n";
+	cout << n << endl;
+}
