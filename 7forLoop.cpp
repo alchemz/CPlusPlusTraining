37813528bf48d8c6d2f092200662c065ee95275a
xianling zhang <xianlingzhang6@gmail.com>
2017-01-03 23:10:20 -0500

    Create 7forLoop.cpp

---
+++ 7forLoop.cpp
@@ -0,0 +1,19 @@
+/*
+for loop
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "The i value is " << i ;
+		cout << "The program runs " << i+1 << "times" << endl;
+	}
+
+
+	return 0;
+}
