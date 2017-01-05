27fe51ceede66490c882a339fe64aaa130c68b2a
xianling zhang <xianlingzhang6@gmail.com>
2017-01-04 19:25:39 -0500

    Create utils.h

---
+++ MultipleFunctionsWithHeader/utils.h
@@ -0,0 +1,21 @@
+
+#ifndef UTILS_H
+#define UTILS_H
+//define and ifndef are also proprecessor
+//use the name of the header
+//if not defined, define it, and do the following
+
+
+//group the function with this class
+//public to make it accessible
+class utils
+{
+public:
+	void speak();
+	void jump();
+};
+
+#endif
+
+
+//this is the proprecessing program
