f657f868bbbce3335fbcfed752a86c9ec86e8432
xianling zhang <xianlingzhang6@gmail.com>
2017-01-04 19:11:37 -0500

    Create utils.cpp

---
+++ header/utils.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "utils.h"
+using namespace std;
+//inclde is the most directive proprecessor
+int main()
+{
+	doSomething();
+	return 0;
+}
+
+void doSomething()
+{
+	cout << "Today is a nice day\n";
+}
