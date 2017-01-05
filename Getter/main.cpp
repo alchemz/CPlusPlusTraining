e208ebf5d9582c7c4e2e0b2be2841afc25c0158a
xianling zhang <xianlingzhang6@gmail.com>
2017-01-04 22:27:24 -0500

    Create main.cpp

---
+++ Getter/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include "Person.h"
+
+using namespace std;
+
+int main()
+{
+    PERSON_N personName;
+    personName.getName();
+    personName.toString();
+    
+    return 0;
+    
+}
