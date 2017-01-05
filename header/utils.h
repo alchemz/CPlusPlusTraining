c5f93e0d7a5dcd175be8b0f85cda9c0e857cb850
xianling zhang <xianlingzhang6@gmail.com>
2017-01-04 19:11:26 -0500

    Create utils.h

---
+++ header/utils.h
@@ -0,0 +1,13 @@
+
+#ifndef UTILS_H
+#define UTILS_H
+//define and ifndef are also proprecessor
+//use the name of the header
+//if not defined, define it, and do the following
+
+void doSomething();
+
+#endif
+
+
+//this is the proprecessing program
