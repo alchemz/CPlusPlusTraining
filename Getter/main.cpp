0334f48b315a8f46a24e0c16dbdef4b07d314faf
xianling zhang <xianlingzhang6@gmail.com>
2017-01-04 22:43:16 -0500

    Update main.cpp

--- Getter/main.cpp
+++ Getter/main.cpp
@@ -6,10 +6,15 @@ using namespace std;
 
 int main()
 {
-    PERSON_N personName;
-    personName.getName();
-    personName.toString();
-    
-    return 0;
-    
+	PERSON_N personName;
+	personName.setAName("Ling");
+	personName.toString();
+
+	cout << "Name entered is: " << personName.toString() << endl;
+
+	//use get method
+	personName.getName();
+	cout << "Name entered is: " << personName.getName() << endl;
+	return 0;
+
 }
