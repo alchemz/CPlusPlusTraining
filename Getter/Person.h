2348dc1527bca7312730a8230ca697c26009e1ea
xianling zhang <xianlingzhang6@gmail.com>
2017-01-04 22:43:05 -0500

    Update Person.h

--- Getter/Person.h
+++ Getter/Person.h
@@ -1,16 +1,22 @@
 #ifndef PERSON_H
 #define PERSON_H
+
 #include <iostream>
 #include <string>
 using namespace std;
 
 class PERSON_N
 {
-  private:
-    string name;
-  public:
-    string toString(); //return the string name
-    void getName(); //get the name of string
+private:
+	string name; //declare a variables
+public:
+	string toString(); //return the string name
+	void setAName(string newName); //get the name of string
+	
+	//use get method 
+	string getName();
+	PERSON_N();
 };
 
 #endif
+
