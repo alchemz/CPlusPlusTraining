28d23f88b091bcdaf55f400b460921437a2ebd1b
xianling zhang <xianlingzhang6@gmail.com>
2017-01-04 22:23:34 -0500

    Create Person.h

---
+++ Getter/Person.h
@@ -0,0 +1,13 @@
+#ifndef PERSON_H
+#define PERSON_H
+
+class PERSON_N
+{
+  private:
+    string name;
+  public:
+    string toString(); //return the string name
+    void getName(); //get the name of string
+};
+
+#endif
