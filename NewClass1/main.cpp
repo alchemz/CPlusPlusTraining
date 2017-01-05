e73efb5cd82ecddec83a28b28a514870f963344e
xianling zhang <xianlingzhang6@gmail.com>
2017-01-04 21:17:05 -0500

    Create main.cpp

---
+++ NewClass1/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Cat.h"
+using namespace std;
+
+int main()
+{
+
+Cat makeNoise;
+makeNoise.makeHappy;
+makeNoise.speak();
+
+Cat makeSound;
+makeSound.makeSad;
+makeSad.speak();
+
+return 0;
+}
