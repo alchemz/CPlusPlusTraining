b7017030c7b85efd58947b0e655a04da1bf1fd9e
xianling zhang <xianlingzhang6@gmail.com>
2017-01-04 21:30:43 -0500

    Create Cat.h

---
+++ Constructors/Cat.h
@@ -0,0 +1,19 @@
+//bool happy is a data member
+//if not assign value
+//it will just be some garabage memory allocated
+//bool happy=true; a instance
+//or we can use a constructor
+
+#IFNDEF CAT_H
+#DEFINE CAT_H
+
+class CAT
+{
+private: 
+    bool happy;
+public：
+    void speak();
+    CAT();
+    
+};
+#ENDIF
