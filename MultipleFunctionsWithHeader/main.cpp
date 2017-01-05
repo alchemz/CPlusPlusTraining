59ba0a469f50eb4b63ea4229269dad9593641c2a
xianling zhang <xianlingzhang6@gmail.com>
2017-01-04 19:25:51 -0500

    Create main.cpp

---
+++ MultipleFunctionsWithHeader/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "utils.h"
+using namespace std;
+//include is the most directive proprecessor
+
+
+int main()
+{
+	//declare the variables
+	utils Utils;
+
+	Utils.speak();
+	Utils.jump();
+	return 0;
+}
+
