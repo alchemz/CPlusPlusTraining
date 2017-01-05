0a818e1b47acebb8aba57642705ced70d3915b16
xianling zhang <xianlingzhang6@gmail.com>
2017-01-04 22:25:17 -0500

    Create Person.cpp

---
+++ Getter/Person.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Person.h"
+
+using namespace std;
+
+string PERSON_N::toString(string getName)
+{
+    return getName;
+}
+
+void PERSON_N::getName()
+{
+    getName="Ling";
+}
