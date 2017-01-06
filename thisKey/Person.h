c87ddeb3c62f14c4139fd176e668f5c64647926c
xianling zhang <xianlingzhang6@gmail.com>
2017-01-06 18:40:46 -0500

    Update Person.h

--- thisKey/Person.h
+++ thisKey/Person.h
@@ -4,3 +4,23 @@ this->name=name;
 
 this//a pointer variable pointing to the memory location of the object
 */
+
+#ifndef PERSON_H
+#define PERSON_H
+
+
+#include <iostream>
+using namespace std;
+
+class Person
+{
+public: 
+string toString();
+Person();
+
+private:
+int age;
+string name;
+}
+
+#endif
