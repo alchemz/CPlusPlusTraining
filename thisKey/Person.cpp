644a71dd61e4545e6582d11b1401d3ab287377ee
xianling zhang <xianlingzhang6@gmail.com>
2017-01-06 18:42:25 -0500

    Create Person.cpp

---
+++ thisKey/Person.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Person.h"
+using namespace std;
+
+string Person::Person toString()
+{
+    return name;
+}
+
+Person::Person()
+{
+this ->name=name;
+this ->age=age;
+}
