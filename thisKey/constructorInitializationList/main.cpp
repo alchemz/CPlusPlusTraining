3d4c925ec516c090f988965f7320ab0af8b9d628
xianling zhang <xianlingzhang6@gmail.com>
2017-01-06 19:07:26 -0500

    Create main.cpp

---
+++ thisKey/constructorInitializationList/main.cpp
@@ -0,0 +1,11 @@
+/*two string method to combine string and number
+In order to make the program run faster: 
+instead of initialization name the age like this->name=name;
+
+Person::Person(string name, int age):name(name),age(age){}
+Person::Person():name("unnamed"),age(11){}
+ or public:
+ put Person():name("unnamed"), age(11){}
+for not complex parameters
+
+*/
