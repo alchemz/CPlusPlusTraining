4b4c5be0305b330d32168be43420c6b757222708
xianling zhang <xianlingzhang6@gmail.com>
2017-01-08 22:28:24 -0500

    Update main.cpp

--- nameSpace/main.cpp
+++ nameSpace/main.cpp
@@ -2,12 +2,18 @@
 #include "Cat.h"
 #include "Animal.h"
 using namespace std;
-//using namespace cats;
+using namespace cats;
 using namespace xlz;
 int main()
 {
   Cat cat;
   cat.speak();
+  
+  xlz::Cat cat2;
+  cat2.speak();
+  
+  cats::Cat cat3;
+  cat3.speak();
   
   return 0;
 }
