55d0800818b012837508c7cfa2dc0fd7e44dd76b
xianling zhang <xianlingzhang6@gmail.com>
2017-01-08 19:57:02 -0500

    Create allocateMemory.cpp

---
+++ allocateMem/allocateMemory.cpp
@@ -0,0 +1,62 @@
+/*
+Allocate memory
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Animal
+{
+private:
+  string name;
+public:
+  Animal()
+  {
+  cout<<"Animal created from constructor"<<endl;
+  }
+  
+  ~Animal()
+  {
+  cout<<"Animal created from deconstructor"<<endl;
+  }
+  
+  //copy constructor
+ Animal(const Animal&other): name(other.name){
+  cout<<"Animal created from copied constructor"<<endl;
+  }
+  
+  void setName(string name)
+  {
+  this->name=name;
+  }
+  
+  void speak() const{
+  cout<<"My name is:"<<name<<endl;
+  }
+};
+
+/*
+Use pointer
+Animal *pAnimal=new Animal;
+delete Animal;
+
+Use premete type
+int *pInt = new Int;
+*pInt=8;
+*/
+
+int main()
+{
+//allocate the entire array
+Animal *pAnimal= new Animal[10]; //how many objects, how many animals created
+pAnimal[5].setName("Geogre"); //allocate the first six
+
+pAnimal[5].speak();
+//when free memory, put []Animal
+free [] pAnimal;
+
+char *pMen= new char[1000]; //char is one byte, allocated 1000 bytes;
+delete []pMen;
+return 0;
+}
